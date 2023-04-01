0b9f713f18eca674549bbf71b56285e33c63d37b
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2023-04-01 19:21:44 +0200

    work on ex 3

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -3,10 +3,21 @@ import scala.jdk.javaapi.OptionConverters
 import u04lab.polyglot.OptionToOptional
 import u04lab.code.Option
 import u04lab.code.Option.*
+import scala.util.Random
+import u04lab.code.List
+import u04lab.code.List.*
+import u04lab.code.Stream
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
+
+  private val _r: Random = Random()
+  // when constructed fill with "mines" pairs of random ints in range(0, size - 1)
+  private val _minesSet: List[(Int, Int)] = ???
+  private val _selected: List[(Int, Int)] = Nil()
+
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
     OptionToOptional(None()) // Option => Optional converter
 
-  def won = false
+  def won(): Boolean = length(_minesSet) + length(_selected) == size*size
+
