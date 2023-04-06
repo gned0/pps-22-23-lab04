2301722843defa10dfd55cbf5ba4e01569f9304d
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2023-04-06 16:11:13 +0200

    partial rework of ex3 solution

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -12,24 +12,32 @@ class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
 
   private val _r: Random = Random()
   // when constructed, fill with "mines" pairs of random ints in range(0, size - 1)
-  private val _minesList: List[(Int, Int)] =
+  /* private val _minesList: List[(Int, Int)] =
     Stream.toList(
       Stream.take(Stream.iterate((0, 0))(_ => (_r.nextInt(size-1), _r.nextInt(size-1)))
       )(mines)
-    )
+    ) */
+  private var _minesList: List[(Int, Int)] = Nil()
   private var _selected: List[(Int, Int)] = Nil()
+  while(List.length(_minesList) != mines)
+    _minesList = Cons((_r.nextInt(size-1), _r.nextInt(size-1)), _minesList)
 
-  def neighbors(x: Int, y: Int): Int =
-    var count = 0
+  private def computeNeighbors(x: Int, y: Int): List[(Int, Int)] =
+    var neighbors: List[(Int, Int)] = Nil()
     for(i <- x - 1 to x + 1)
       for(j <- y - 1 to y + 1)
-        if List.contains(_minesList, (i, j)) then count = count + 1
-    count
+          neighbors = Cons((i,j), neighbors)
+    neighbors
+
 
   def hit(x: Int, y: Int): java.util.Optional[Integer] = (x, y) match
     case (_, _) if List.contains(_minesList, (x, y)) => OptionToOptional(None())
     case _ => _selected = Cons((x, y), _selected)
-              OptionToOptional(Some(neighbors(x, y)))
+              val neighbors = computeNeighbors(x, y)
+              val adjMines = List.length(List.filter(neighbors)(e => List.contains(_minesList, e)))
+              if(adjMines == 0)
+                ???
+              OptionToOptional(Some(adjMines))
 
   def won(): Boolean = length(_minesList) + length(_selected) == size*size
 
